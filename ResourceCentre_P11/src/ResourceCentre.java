4bb16bab38ce077ab6182a07551201e213bc2e0c
weihong <20023766@rp.edu.sg>
2021-08-03 11:26:23 +0800

    Refractoring

--- ResourceCentre_P11/src/ResourceCentre.java
+++ ResourceCentre_P11/src/ResourceCentre.java
@@ -132,7 +132,7 @@ public class ResourceCentre {
 			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", camcorderList.get(i).getAssetTag(),
 					camcorderList.get(i).getDescription(), 
 					ResourceCentre.showAvailability(camcorderList.get(i).getIsAvailable()),
-					camcorderList.get(i).getDueDate(),camcorderList.get(i).getOpticalZoom());
+					camcorderList.get(i).getDueDate(),camcorderList.get(i).getZoom());
 		}
 		return output;
 	}
